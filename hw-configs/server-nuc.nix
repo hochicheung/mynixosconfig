32983d0146746a8633d35e21d349d8dbbeb906c1
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-10-27 23:08:32 +0200

    first nuc prototype

--- hw-configs/server-nuc.nix
+++ hw-configs/server-nuc.nix
@@ -2,9 +2,13 @@
 
 {
   imports = [
-	  ./generic/server.nix
 		../system-modules/boot/uefi.nix
+		../system-modules/auto-upgrade-server.nix
+
+		../system-modules/hardware/cpu-firmware/intel.nix
+		../system-modules/hardware/SSD.nix
 
 		../system-modules/syncthing.nix
+		../system-modules/network/sshd.nix
   ];
 }
