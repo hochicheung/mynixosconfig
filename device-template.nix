cc711e4cd8210c5635c12a55af8b9f9383b75905
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-10-29 02:00:36 +0200

    fixed parens

--- device-template.nix
+++ device-template.nix
@@ -9,35 +9,36 @@ in
 		# ./system-modules/network/sshd.nix
 	];
 
-networking.hostName = "nixos";
-
-users = {
-	users.samcheung = {
-	  group = "users";
-		isNormalUser = true;
-		uid = 1000;
-		hashedPassword = pass;
-		extraGroups = [ "wheel" "networkmanager" "video"];
-	};
-
-	users.root = {
-	  hashedPassword = pass;
-	};
-
-	mutableUsers = false;
-};
-
-# SSH
-services.openssh.listenAddresses = [
-  {
-	  addr = "192.168.1.1"; port = 22;
-  }
-];
-
-# Syncthing
-services.syncthing.relay = {
-  enable = true;
-	port = ;
-	statusPort = ;
-	networking.firewall.allowedTCPPorts = [];
-};
+  networking.hostName = "nixos";
+  
+  users = {
+	  users.samcheung = {
+	    group = "users";
+		  isNormalUser = true;
+		  uid = 1000;
+		  hashedPassword = pass;
+		  extraGroups = [ "wheel" "networkmanager" "video"];
+	  };
+  
+	  users.root = {
+	    hashedPassword = pass;
+	  };
+  
+	  mutableUsers = false;
+  };
+  
+  # SSH
+  services.openssh.listenAddresses = [
+    {
+	    addr = "192.168.1.1"; port = 22;
+    }
+  ];
+  
+  # Syncthing
+  services.syncthing.relay = {
+    enable = true;
+	  port = ;
+	  statusPort = ;
+	  networking.firewall.allowedTCPPorts = [];
+  };
+}
