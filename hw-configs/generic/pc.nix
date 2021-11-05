1793e95c037b104e895123ec93de646c76fabb6c
Sam Cheung <hochicheung@gmail.com>
2021-11-05 15:45:59 +0100

    hw-config changes

--- hw-configs/generic/pc.nix
+++ hw-configs/generic/pc.nix
@@ -4,13 +4,20 @@
 
 	imports = [
 	  ../../system-modules/xorg/xorg.nix
+
 	  ../../system-modules/audio/pulse.nix
 	  # ../../system-modules/audio/pipewire.nix
+
 	  ../../system-modules/wakelock.nix
-	  #../../system-modules/hardware/file-systems/ntfs-support.nix
+
 	  ../../system-modules/hardware/input/input.nix
-	  #../../system-modules/auto-upgrade.nix
+
 	  ../../system-modules/fonts/font.nix
+
+	  #../../system-modules/hardware/file-systems/ntfs-support.nix
+
+	  #../../system-modules/auto-upgrade.nix
+
 	  #../../system-modules/polkit.nix
 	];
 }
--- hw-configs/server-nuc.nix
+++ hw-configs/server-nuc.nix
@@ -2,19 +2,19 @@
 
 {
   imports = [
+		../hw-configs/generic/pc.nix
+		../myprivatenixosconfig/server.nix
+
 		../system-modules/boot/uefi.nix
 		../system-modules/auto-upgrade-server.nix
 
 		../system-modules/hardware/cpu-firmware/intel.nix
 		../system-modules/hardware/SSD.nix
 
-		../system-modules/xorg/xorg.nix
-		../system-modules/hardware/input/input.nix
-
 		../system-modules/syncthing.nix
+
 		../system-modules/network/sshd.nix
 		../system-modules/network/sshd-xorg.nix
 
-		../myprivatenixosconfig/server.nix
   ];
 }
