9961d1a026bfde322ea6e5d5cc1d66dfad958570
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-11-18 00:31:00 +0100

    added hibernation

--- hw-configs/generic/laptop.nix
+++ hw-configs/generic/laptop.nix
@@ -5,7 +5,7 @@
 		./pc.nix
 	  ../../modules/system-modules/hardware/SSD.nix
 	  ../../modules/system-modules/hardware/backlight.nix
-		../../modules/system-modules/hardware/screenpowersetting.nix
+		../../modules/system-modules/hibernation.nix
 	];
 
 	services.tlp.enable = true;
--- modules/system-modules/hibernation.nix
+++ modules/system-modules/hibernation.nix
@@ -1,13 +1,24 @@
 { config, pkgs, ... }:
 {
-  # Only turn off screen on lid close
+	imports = [
+	  ./wakelock.nix
+	];
+
+  # Ignore closing lid event
   services.logind.lidSwitch = "ignore";
   services.logind.lidSwitchDocked = "ignore";
   
-  # Powerswitch locks, Idle 
   services.logind.extraConfig = ''
     HandlePowerKey=suspend
+
+		# When idle, suspend
     IdleAction=suspend
-    # IdleActionSec=30
+    IdleActionSec=60
   '';
+
+	programs.xss-lock = {
+	  enable = true;
+
+		lockerCommand = "physlock";
+	};
 }
