32e141dcdcafcf481119936dcbc0827159632cb0
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-12-16 16:50:24 +0100

    update template

--- device/.home-device-template.nix
+++ device/.home-device-template.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 {
   imports = [
-    # ./home-modules/email.nix
-    # ./home-modules/games/cataclysm-dda/cataclysm-dda-git-latest.nix
-    # ./home-modules/emacs.nix
-    # ./home-modules/art.nix
+    # ../modules/home-modules/email.nix
+    # ../modules/home-modules/games/cataclysm-dda/cataclysm-dda-git-latest.nix
+    # ../modules/home-modules/emacs.nix
+    # ../modules/home-modules/art.nix
   ];
 
   # home.packages = with pkgs; [
