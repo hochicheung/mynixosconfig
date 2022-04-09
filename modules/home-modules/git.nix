1699cf03bcb9814699ff497117d6a99d6b1da3d0
Sam Cheung <hochi.cheung.sam@gmail.com>
2022-04-09 16:55:50 +0200

    Changed git.nix

--- modules/home-modules/git.nix
+++ modules/home-modules/git.nix
@@ -2,7 +2,9 @@
 {
   programs.git = {
     enable = true;
-    userName = "Sam Cheung";
-    userEmail = "hochi.cheung.sam@gmail.com";
+		extraConfig = {
+		  user = { name = "Sam Cheung"; email = "hochi.cheung.sam@gmail.com"; };
+			submodule = { recurse = true; };
+		};
   };
 }
