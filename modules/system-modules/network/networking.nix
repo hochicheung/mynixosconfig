4a6a16e320634d896b349b1a263c8218939164a3
Sam Cheung <hochi.cheung.sam@gmail.com>
2023-03-14 09:53:58 +0100

    Networking config

---
+++ modules/system-modules/network/networking.nix
@@ -0,0 +1,12 @@
+{
+	networking.wireless.enable = true;
+	networking.wireless.userControlled.enable = true;
+
+	# networking.wireless.environmentFile = "/etc/nixos/modules/myprivatenixosconfig/secrets/wireless.env";
+	# networking.wireless.networks = {
+	  # hotspot.psk = ''
+		  # password = "@PERS_HOTSPOT@";
+			# priority = 0;
+		# '';
+	# };
+}
