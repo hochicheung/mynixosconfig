fc829aa23ba5eb4655ac044fbe4eeb0984a50ecf
Sam Cheung <hochi.cheung.sam@gmail.com>
2023-03-19 16:03:52 +0100

    Changed networking

--- modules/system-modules/network/networking.nix
+++ modules/system-modules/network/networking.nix
@@ -4,17 +4,7 @@
 
 	networking.wireless.environmentFile = "/etc/nixos/modules/myprivatenixosconfig/secrets/wireless.env";
 
-	networking.wireless.networks = {
-		"SamC-Network".psk = "@PERS_HOTSPOT@";
-		"SamC-Network".priority = 2;
-
-		"eduroam".auth = ''
-		  key_mgmt=WPA-EAP
-			eap=PEAP
-			identity="samch@kth.se"
-			password="@KTH_EDUROAM@"
-			phase2="auth=MSCHAPV2"
-		'';
-		"eduroam".priority = 1;
-	};
+	imports = [
+	 	../../myprivatenixosconfig/secrets/wireless-networks.nix
+	];
 }
