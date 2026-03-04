e421e1a6bdd59d2c3a4040393ac4a463f7ac29a3
Sam Cheung <hochi.cheung.sam@gmail.com>
2026-03-04 18:11:11 +0100

    Flake file have declarations of users

--- flake.nix
+++ flake.nix
@@ -10,14 +10,18 @@
 	    # to avoid problems caused by different versions of nixpkgs.
 	    inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ...}: {
+    # my-secrets.url = "git+ssh://git@github.com:user/privaterepo.git";
+  };
+  # outputs = { self, nixpkgs, home-manager, my-secrets, ...}: {
+
+  outputs = { self, nixpkgs, home-manager, ...}: {
     nixosConfigurations = {
+      # PWNI TP250
 	    pwni = nixpkgs.lib.nixosSystem {
 	      system = "x86_64-linux";
 	      modules = [
-	        ./configuration.nix
+          /etc/nixos/private-nixos-config/user/pwni/system.nix
    
 		      #	make home-manager as a module of nixos
 		      # so that home-manager configuration will be deployed
@@ -27,12 +31,36 @@
 		      {
 		        home-manager.useGlobalPkgs = true;
 			      home-manager.useUserPackages = true;
-			      home-manager.users.samcheung = import ./private-nixos-config/user/laptop-tp250/home.nix;
+            # home-manager.extraSpecialArgs = { inherit my-secrets; };
 			      # Optionally use home-manager.extraSpecialArgs to pass
 			      # arguments to home.nix
+
+			      home-manager.users.samcheung = import ./private-nixos-config/user/pwni/home.nix;
+		      }
+		    ];
+	    };
+      # RMAN = MSI
+	    rman = nixpkgs.lib.nixosSystem {
+	      system = "x86_64-linux";
+	      modules = [
+          /etc/nixos/private-nixos-config/user/rman/system.nix
+           
+		      #	make home-manager as a module of nixos
+		      # so that home-manager configuration will be deployed
+		      # automatically when executing `nixos-rebuild`
+  
+          home-manager.nixosModules.home-manager
+		      {
+		        home-manager.useGlobalPkgs = true;
+			      home-manager.useUserPackages = true;
+            # home-manager.extraSpecialArgs = { inherit my-secrets; };
+			      # Optionally use home-manager.extraSpecialArgs to pass
+			      # arguments to home.nix
+
+			      home-manager.users.samcheung = import ./private-nixos-config/user/rman/home.nix;
 		      }
 		    ];
 	    };
     };
   };
-}
+
